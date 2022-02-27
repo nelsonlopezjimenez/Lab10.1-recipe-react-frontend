cef38c89c7a6807d2e414efd093fca965100f769
Nelson as studentalfa <nelson.lopezjimenez@seattlecolleges.edu>
2022-02-26 19:12:46 -0800

    List from class compnt to function compnt

--- src/List.js
+++ src/List.js
@@ -1,22 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Recipe from './Recipe';
 import './List.css';
 
-class List extends Component {
-
-  render() {
-    const {onDelete} = this.props;
-    const recipes = this.props.recipes.map((r) => (
-      <Recipe key={r._id} {...r} onDelete={onDelete} />
-    ));
-
-    return (
-      <div className="recipe-list">
-        {recipes}
-      </div>
-    )
-
-  }
+function List (props){
+  const {onDelete} = props;
+  const recipes = props.recipes.map( recipe => (
+    <Recipe key={recipe._id} {...recipe} onDelete={onDelete} />
+  ));
+  return(
+    <div className='recipe-list'>
+       {recipes}
+    </div>
+  )
 }
 
 export default List;
