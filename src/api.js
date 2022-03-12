8c734cceef816807367d4e65abcd0c3f0ce68e7f
Nelson as studentalfa <nelson.lopezjimenez@seattlecolleges.edu>
2022-03-11 17:05:43 -0800

    api modified no catch block

--- src/api.js
+++ src/api.js
@@ -1,69 +1,72 @@
 const APIURL = '/api/recipes/';
 
+
 export async function getRecipes() {
+
   return fetch(APIURL)
-    .then(resp => {
-      if (!resp.ok) {
-        if (resp.status >= 400 && resp.status < 500) {
-          return resp.json().then(data => {
-            let err = { errorMessage: data.message };
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      else {
+        if (!response.ok) {
+          if (response.status >= 400 && response.status < 500) {
+            return response.json().then(data => {
+              let err = { errorMessage: data.message };
+              throw err;
+            })
+          }
+          else {
+            let err = { errorMessage: 'Please try again later, server is not responding' };
             throw err;
-          })
-        } else {
-          let err = { errorMessage: 'Please try again later, server is not responding' };
-          throw err;
+          }
         }
-      } else {
-        console.log("getRecipes: everything is fine");
-        //shows the promise 
+      }
+    })
+  }
+
+  export async function createRecipe(recipe) {
+    return fetch(APIURL, {
+      method: 'post',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify(recipe)
+    })
+      .then(resp => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then(data => {
+              let err = { errorMessage: data.message };
+              throw err;
+            })
+          } else {
+            let err = { errorMessage: 'Please try again later, server is not responding' };
+            throw err;
+          }
+        }
         return resp.json();
-      }
-      //console.log("getRecipes resp: ", resp.json())
-      
-    });
-}
-
-export async function createRecipe(recipe) {
-  return fetch(APIURL, {
-    method: 'post',
-    headers: new Headers({
-      'Content-Type': 'application/json',
-    }),
-    body: JSON.stringify(recipe)
-  })
-    .then(resp => {
-      if (!resp.ok) {
-        if (resp.status >= 400 && resp.status < 500) {
-          return resp.json().then(data => {
-            let err = { errorMessage: data.message };
-            throw err;
-          })
-        } else {
-          let err = { errorMessage: 'Please try again later, server is not responding' };
-          throw err;
-        }
-      }
-      return resp.json();
+      })
+  }
+  
+  export async function removeRecipe(id) {
+    const deleteURL = APIURL + id;
+    return fetch(deleteURL, {
+      method: 'delete'
     })
-}
-
-export async function removeRecipe(id) {
-  const deleteURL = APIURL + id;
-  return fetch(deleteURL, {
-    method: 'delete'
-  })
-    .then(resp => {
-      if (!resp.ok) {
-        if (resp.status >= 400 && resp.status < 500) {
-          return resp.json().then(data => {
-            let err = { errorMessage: data.message };
+      .then(resp => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then(data => {
+              let err = { errorMessage: data.message };
+              throw err;
+            })
+          } else {
+            let err = { errorMessage: 'Please try again later, server is not responding' };
             throw err;
-          })
-        } else {
-          let err = { errorMessage: 'Please try again later, server is not responding' };
-          throw err;
+          }
         }
-      }
-      return resp.json();
-    })
-}
+        return resp.json();
+      })
+  }
+  
