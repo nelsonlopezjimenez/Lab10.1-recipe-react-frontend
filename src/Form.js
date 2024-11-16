e0a9d3a54085517fb975bd7d316b3c4160f45da7
Creeper Panda T Rex <paintorange-nelson@outlook.com>
2024-11-16 07:28:21 -0800

    Input boxes borders and bg added

--- src/Form.js
+++ src/Form.js
@@ -29,14 +29,14 @@ function Form(props) {
       key={`ingredient-${i}`}
     >
       <label>{i + 1}.
-        <input
+        <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg   w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
           type="text"
           name={`ingredient-${i}`}
           value={ing}
           size={40}
           autoComplete="off"
           placeholder=" Ingredient"
-          onChange={handleChangeIng} />
+          onChange={handleChangeIng} required/>
       </label>
     </div>
   ));
@@ -76,12 +76,17 @@ function Form(props) {
             X
           </li>
         </ul>
-        <form className='recipe-form border-8 border-gray-600 rounded flex flex-col flex-start px-6 m-0' onSubmit={onSave} >
+        <form className='recipe-form border-8 border-gray-600 rounded-lg flex flex-col flex-start px-6 m-0' onSubmit={onSave} >
           {/* <form className='recipe-form' onClick={(e)=> handleAlert(e)}> */}
           {/* <form className='recipe-form' onSubmit={() => alert()}> */}
-          <div className='recipe-form-line'>
+          {/* <div>
+            <label htmlFor="title1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title 1</label>
+            <input type="text" id="title1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg   w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+            key='title1' name='title1' value={oneRecipe.title}  autoComplete="off" onChange={handleChange} placeholder="title" required />
+        </div> */}
+          <div className='recipe-form-line '>
             <label htmlFor='recipe-title-input'>Title</label>
-            <input
+            <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg   w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white '
               id='recipe-title-input'
               key='title'
               name='title'
@@ -89,15 +94,15 @@ function Form(props) {
               value={oneRecipe.title}
               size={40}
               autoComplete="off"
-              onChange={handleChange} />
+              onChange={handleChange} placeholder="title" required  />
           </div>
           <label
-            htmlFor='recipe-instructions-input'
+            htmlFor='recipe-instructions-input block mb-2 text-sm font-medium text-gray-900 dark:text-white'
             style={{ marginTop: '5px' }}
           >
             Instructions
           </label>
-          <textarea
+          <textarea className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg   w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white '
             key='instructions'
             id='recipe-instructions-input'
             type='Instructions'
@@ -106,7 +111,7 @@ function Form(props) {
             cols='50'
             autoComplete='off'
             value={oneRecipe.instructions}
-            onChange={handleChange} />
+            onChange={handleChange} required/>
           {inputs}
           <button
             type="button"
@@ -117,7 +122,7 @@ function Form(props) {
           </button>
           <div className='recipe-form-line'>
             <label htmlFor='recipe-img-input'>Image Url</label>
-            <input
+            <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg   w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white'
               id='recipe-img-input'
               type='text'
               placeholder=''
@@ -125,7 +130,7 @@ function Form(props) {
               value={oneRecipe.img}
               size={36}
               autoComplete='off'
-              onChange={handleChange} />
+              onChange={handleChange} required/>
           </div>
           <button
             className="buttons text-white bg-gray-600 p-l-0 px-4 py-2 mx-4 rounded border-6 border-white"
